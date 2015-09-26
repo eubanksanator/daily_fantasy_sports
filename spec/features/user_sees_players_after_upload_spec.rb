82075eb90f52651fd2e9734be46d6d7fc101917f
Glenn Espinosa <glennpeter.espinosa@gmail.com>
2015-09-26 09:49:38 -0400

    Refactor feature specs
    Refactored the acceptance tests into single level abstraction. This change will improve readibility and improve modularity later on if we need to change something with the dataset creation process. Instead of changing the process at multiple scenarios, we've isolated the create_dataset method with all of the related logic.

--- spec/features/user_sees_players_after_upload_spec.rb
+++ spec/features/user_sees_players_after_upload_spec.rb
@@ -2,17 +2,21 @@ require 'rails_helper'
 
 feature "User imports datasets" do
   scenario "they see the uploaded players" do
-    visit root_path
-
-    click_on("Import new datasets")
-    fill_in("Name", with: "DraftKingsWeek2")
-    attach_file("Salaries", Rails.root + "spec/fixtures/example_salaries.csv")
-    attach_file("Rankings", Rails.root + "spec/fixtures/example_rankings.csv")
-    click_on("Import")
+    create_dataset("DraftKingsWeek2")
 
     expect(page).to have_content("DraftKingsWeek2")
     expect(page).to have_content("Drew Brees")
     expect(page).to have_content("Andrew Luck")
     expect(page).to have_content("383.4")
+  end
+
+  def create_dataset(name)
+    visit root_path
+
+    click_on("Import new datasets")
+    fill_in("Name", with: name)
+    attach_file("Salaries", Rails.root + "spec/fixtures/example_salaries.csv")
+    attach_file("Rankings", Rails.root + "spec/fixtures/example_rankings.csv")
+    click_on("Import")
   end
 end
--- spec/features/user_uploads_datasets_spec.rb
+++ spec/features/user_uploads_datasets_spec.rb
@@ -2,51 +2,48 @@ require 'rails_helper'
 
 feature "User imports datasets" do
   scenario "successfully" do
-    visit root_path
+    create_dataset(name: "DraftKingsWeek2", salaries: "example_salaries.csv", rankings: "example_rankings.csv")
 
-    click_on("Import new datasets")
-    fill_in("Name", with: "DraftKingsWeek2")
-    attach_file("Salaries", Rails.root + "spec/fixtures/example_salaries.csv")
-    attach_file("Rankings", Rails.root + "spec/fixtures/example_rankings.csv")
-    click_on("Import")
-
-    expect(page).to have_css('p', text: 'Datasets imported successfully!')
+    expect(page).to have_message('Datasets imported successfully!')
     expect(Dataset.where(salaries_filename: "example_salaries.csv", rankings_filename: "example_rankings.csv")).to exist
   end
 
   scenario "unsuccessfully due to missing a Salary Dataset" do
-    visit root_path
+    create_dataset(name: "DraftKingsWeek2", rankings: "example_rankings.csv")
 
-    click_on("Import new datasets")
-    fill_in("Name", with: "DraftKingsWeek2")
-    attach_file("Salaries", Rails.root + "spec/fixtures/example_salaries.csv")
-    click_on("Import")
-
-    expect(page).to have_css('p', text: 'All form fields must be completed!')
+    expect(page).to have_message('All form fields must be completed!')
     expect(Dataset.where(salaries_filename: "example_salaries.csv")).not_to exist
   end
 
   scenario "unsuccessfully due to missing a Rankings Dataset" do
-    visit root_path
+    create_dataset(name: "DraftKingsWeek2", salaries: "example_salaries.csv")
 
-    click_on("Import new datasets")
-    fill_in("Name", with: "DraftKingsWeek2")
-    attach_file("Rankings", Rails.root + "spec/fixtures/example_rankings.csv")
-    click_on("Import")
-
-    expect(page).to have_css('p', text: 'All form fields must be completed!')
+    expect(page).to have_message('All form fields must be completed!')
     expect(Dataset.where(rankings_filename: "example_rankings.csv")).not_to exist
   end
 
   scenario "unsuccessfully due to missing a name" do
+    create_dataset(salaries: "example_salaries.csv", rankings: "example_rankings.csv")
+
+    expect(page).to have_message('All form fields must be completed!')
+    expect(Dataset.where(rankings_filename: "example_rankings.csv")).not_to exist
+  end
+
+  def create_dataset(args)
+    name     = args[:name]
+    rankings = args[:rankings]
+    salaries = args[:salaries]
+
     visit root_path
 
     click_on("Import new datasets")
-    attach_file("Rankings", Rails.root + "spec/fixtures/example_rankings.csv")
-    attach_file("Salaries", Rails.root + "spec/fixtures/example_salaries.csv")
+    fill_in("Name", with: name) if name.present?
+    attach_file("Salaries", Rails.root + "spec/fixtures/#{salaries}") if salaries.present?
+    attach_file("Rankings", Rails.root + "spec/fixtures/#{rankings}") if rankings.present?
     click_on("Import")
+  end
 
-    expect(page).to have_css('p', text: 'All form fields must be completed!')
-    expect(Dataset.where(rankings_filename: "example_rankings.csv")).not_to exist
+  def have_message(message)
+    have_css('p', text: message)
   end
 end
