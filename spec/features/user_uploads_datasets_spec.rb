a2399d80c6db43ca50d01f75f086ce83212ef31c
Glenn Espinosa <glennpeter.espinosa@gmail.com>
2015-09-27 12:09:27 -0400

    Extract visiting new dataset page into own method
    This small refactor extracts the logic for visiting the new_dataset_path into it's own method. This will allow for easier future changes to that process while also increasing readibility.

--- spec/features/user_uploads_datasets_spec.rb
+++ spec/features/user_uploads_datasets_spec.rb
@@ -2,28 +2,28 @@ require 'rails_helper'
 
 feature "User imports datasets" do
   scenario "successfully" do
-    create_dataset(name: "DraftKingsWeek2", salaries: "example_salaries.csv", rankings: "example_rankings.csv")
+    create_dataset(name: "DraftKingsWeek2", salaries_file: "example_salaries.csv", rankings_file: "example_rankings.csv")
 
     expect(page).to have_message('Datasets imported successfully!')
     expect(Dataset.where(salaries_filename: "example_salaries.csv", rankings_filename: "example_rankings.csv")).to exist
   end
 
   scenario "unsuccessfully due to missing a Salary Dataset" do
-    create_dataset(name: "DraftKingsWeek2", rankings: "example_rankings.csv")
+    create_dataset(name: "DraftKingsWeek2", rankings_file: "example_rankings.csv")
 
     expect(page).to have_message('All form fields must be completed!')
     expect(Dataset.where(salaries_filename: "example_salaries.csv")).not_to exist
   end
 
   scenario "unsuccessfully due to missing a Rankings Dataset" do
-    create_dataset(name: "DraftKingsWeek2", salaries: "example_salaries.csv")
+    create_dataset(name: "DraftKingsWeek2", salaries_file: "example_salaries.csv")
 
     expect(page).to have_message('All form fields must be completed!')
     expect(Dataset.where(rankings_filename: "example_rankings.csv")).not_to exist
   end
 
   scenario "unsuccessfully due to missing a name" do
-    create_dataset(salaries: "example_salaries.csv", rankings: "example_rankings.csv")
+    create_dataset(salaries_file: "example_salaries.csv", rankings_file: "example_rankings.csv")
 
     expect(page).to have_message('All form fields must be completed!')
     expect(Dataset.where(rankings_filename: "example_rankings.csv")).not_to exist
@@ -31,16 +31,20 @@ feature "User imports datasets" do
 
   def create_dataset(args)
     name     = args[:name]
-    rankings = args[:rankings]
-    salaries = args[:salaries]
+    rankings_file = args[:rankings_file]
+    salaries_file = args[:salaries_file]
 
-    visit root_path
+    visit_new_dataset_page
 
-    click_on("Import new datasets")
-    fill_in("Name", with: name) if name.present?
-    attach_file("Salaries", Rails.root + "spec/fixtures/#{salaries}") if salaries.present?
-    attach_file("Rankings", Rails.root + "spec/fixtures/#{rankings}") if rankings.present?
+    fill_in("Name", with: name) if name
+    attach_file("Salaries", Rails.root + "spec/fixtures/#{salaries_file}") if salaries_file
+    attach_file("Rankings", Rails.root + "spec/fixtures/#{rankings_file}") if rankings_file
     click_on("Import")
+  end
+
+  def visit_new_dataset_page
+    visit root_path
+    click_on("Import new datasets")
   end
 
   def have_message(message)
