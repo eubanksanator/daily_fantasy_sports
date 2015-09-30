ff93beecbd0d72e1972c5862ad5408ad28d5c574
Glenn Espinosa <glennpeter.espinosa@gmail.com>
2015-09-30 15:08:38 -0400

    Add initial specs for combinator class
    Initially tried to use FactoryGirl for Combinator but it was more trouble that it was worth. The initialization tests for number of arguments is not needed now that the Combinator class is being initialized with a arguments hash. No point in checking for the bad path regarding arguments at this point. On to the business logic of forming lineup combinations.

--- app/models/combinator.rb
+++ app/models/combinator.rb
@@ -1,9 +1,18 @@
 class Combinator
-  attr_reader :data
+  attr_reader :data, :config
 
-  def initialize(data, config)
-    @data = data
-    @config = config
+  def initialize(args)
+    default_config = {
+                       "QB"   => 1,
+                       "RB"   => 2,
+                       "WR"   => 3,
+                       "TE"   => 1,
+                       "Flex" => 1,
+                       "DST"  => 1,
+                     }
+
+    @data   = args[:data]
+    @config = args[:config] || default_config
   end
 
   def output_data
--- spec/models/combinator_spec.rb
+++ spec/models/combinator_spec.rb
@@ -1,22 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe Combinator do
-  let(:lineup) { {
-                   "QB"   => 1,
-                   "RB"   => 2,
-                   "WR"   => 3,
-                   "TE"   => 1,
-                   "Flex" => 1,
-                   "DST"  => 1
-                 } }
+  let(:lineup) do
+    {
+      "QB"   => 1,
+      "RB"   => 2,
+      "WR"   => 3,
+      "TE"   => 1,
+      "Flex" => 1,
+      "DST"  => 1
+    }
+  end
 
   before :each do
-    @combinator = Combinator.new(Player.all, lineup)
+    @combinator = Combinator.new(data: Player.all, config: lineup)
   end
 
   describe "validations" do
     it "accepts data and a configuration" do
       expect(@combinator).to be_a(Combinator)
+    end
+
+    it "is valid without a specific lineup" do
+      combinator = Combinator.new(data: Player.all)
+      expect(combinator).to be_a(Combinator)
+      expect(combinator.config).to eq(lineup)
     end
   end
 
